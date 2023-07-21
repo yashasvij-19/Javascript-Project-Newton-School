a618bafdf77d878738b3d74f2084613a32606a38
Yashasvi <yashasvivij01@gmail.com>
2023-07-21 17:14:24 +0530

    No repeat searches stored

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var histArr = [];
+let present = false;
 
 async function getData(){
     let word = document.getElementById("searchInput").value;
@@ -7,22 +8,28 @@ async function getData(){
 
     let mainDiv = document.querySelector(".meaningCard");
     mainDiv.textContent = "Meaning:  " + data[0].meanings[0].definitions[0].definition;
-    mainDiv.style.marginLeft = "32%";
-    mainDiv.style.padding = "10px";
-    mainDiv.style.fontSize = "17px";
-    mainDiv.style.fontWeight = "700";
-    mainDiv.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif";
-    mainDiv.style.color = "rgb(43,42,42)";
-    mainDiv.style.width = "30%";
 
+    let dataGet = JSON.parse('[' + localStorage.getItem("history") + ']');
+    dataGet.map((el)=>{
+if(el.meaning === data[0].meanings[0].definitions[0].definition){
+    present = true;
+}
+    });
+
+if(!present){
 
     let obj = {};
     obj.id = histArr.length;
     obj.word = word;
     obj.meaning = data[0].meanings[0].definitions[0].definition;
+
     histArr.push(JSON.stringify(obj));
     localStorage.setItem("history",histArr);
+    present = false;
 }
+
+}
+
 
 function history(){
     let theData = JSON.parse('[' + localStorage.getItem("history") + ']');
@@ -33,36 +40,25 @@ function history(){
         let btn = document.createElement("button");
         p1.innerText = "word : " + el.word;
         p2.innerText = el.meaning;
-        btn.innerHTML = `<i class="fa fa-trash-o" style="font-size:35px"></i>`;
-        btn.style.cursor = "pointer";
-        btn.style.backgroundColor = "white";
-        div.style.backgroundColor = "white";
-        btn.style.color = "black";
-        div.style.width = "200px";
-        btn.style.width = "50px";
-        div.style.height = "250px";
-        btn.style.height = "50px";
-        div.style.margin = "10px";
-        div.style.padding = "10px";
-        div.style.border = "1px solid black";
+        btn.innerHTML = `<i class="fa fa-trash-o" style="font-size:24px"></i>`;
         btn.className = "theBtn";
-        p1.style.fontSize = "17px";
-        p2.style.fontSize = "15px";
-        p1.style.fontWeight = "700";
-        p1.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif";
-        p2.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif";
-        p1.style.color = "rgb(43,42,42)";
-        p2.style.color = "rgb(43,42,42)";
+        div.className = "card";
+        p1.className = "title";
+        p2.className = "description";
 
         div.appendChild(p1);
         div.appendChild(p2);
         div.appendChild(btn);
 
-        document.querySelector(".historyCards").append(div);
+        document.querySelector(".historyCards").appendChild(div);
 
         document.querySelector(".theBtn").addEventListener("click",()=>{
-            histArr = [];
             let theData = JSON.parse('[' + localStorage.getItem("history") + ']');
+            histArr = [];
+            if(theData.length == 1){
+                document.querySelector(".historyCards").textContent = "";
+                localStorage.setItem("history",histArr);
+            }else{
             document.querySelector(".historyCards").textContent = "";
             for(var i = 0; i<theData.length; i++){
                 if(i !== el.id){
@@ -71,6 +67,7 @@ function history(){
             }
             localStorage.setItem("history",histArr);
             history();
+        }
         })
     })
        
